b85222ede36dedef812b02c0fd36a4fc2ad43624
Alexander Komarov <komar81@gmail.com>
2020-12-03 10:44:18 +0300

    module3-task3

---
+++ src/service/cli/server/server.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const chalk = require(`chalk`);
+const express = require(`express`);
+const {API_DEFAULT_PORT} = require(`../../../constants`);
+const {offersRoutes} = require(`./offers-routes.js`);
+const app = express();
+app.use(express.json());
+app.use(`/offers`, offersRoutes);
+
+module.exports = {
+  name: `--server`,
+  run(args) {
+    const [customPort] = args;
+    const port = Number.parseInt(customPort, 10) || API_DEFAULT_PORT;
+    app.listen(port, (err) =>
+      err
+        ? console.error(chalk.red(`Ошибка при создании сервера`, err))
+        : console.info(chalk.green(`Ожидаю соединений на ${port}`))
+    );
+  },
+};
